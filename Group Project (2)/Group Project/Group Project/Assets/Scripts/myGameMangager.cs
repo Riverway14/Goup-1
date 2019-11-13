cb80ce22f9b17b9e0f373b97dc42df1eadf5fa78
Riverway14 <57182700+Riverway14@users.noreply.github.com>
2019-11-13 11:11:50 -0500

    e

---
+++ Group Project (2)/Group Project/Group Project/Assets/Scripts/myGameMangager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myGameMangager : MonoBehaviour
+{
+    Score count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = FindObjectOfType<Score>();
+        if(PlayerPrefs.HasKey("Score"))
+        {
+            count.SetScore(PlayerPrefs.GetInt("Score"));
+        }
+       // count.getScore(); //had to make pubic or it was inacasse able due to protection level
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
